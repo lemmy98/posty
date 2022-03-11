8d111fb90a51fc8a24e94c885917f634f12757cd
temitope <lemikanemmanuel@gmail.com>
2022-03-11 10:22:04 +0100

    Added laravel blog

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './resources/views/auth/register.blade.php',
+    './resources/views/layouts/app.blade.php',
+    './resources/views/posts/index.blade.php',
+     './resources/js/app.js',
+    //  './resources/**/*.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
